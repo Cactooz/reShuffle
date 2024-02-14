44182ca6e970c96642ec1b20b735c1bf8947acd2
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2024-02-14 13:58:42 +0100

    Update shuffle to work with new fields

--- src/shuffles.js
+++ src/shuffles.js
@@ -3,13 +3,17 @@ import { fetchTracksOfPlaylist } from './fetch';
 export async function spotifyShuffle2014(id, total) {
 	//Fetch tracks
 	const playlist = await fetchTracksOfPlaylist(id, total);
-
+	//console.log('playlist', playlist);
 	//track array containing objects with artist and id
 	const tracks = playlist
-		.filter((trackObject) => !trackObject.is_local) //Remove local and unplayable tracks
+		.filter((trackObject) => !trackObject.track.is_local) //Remove local tracks
 		.map((trackObject) => {
 			//Only keep artist and id of track
-			return { artist: trackObject.track.artists[0].name, id: trackObject.track.id };
+			return {
+				artist: trackObject.track.artists[0].name,
+				id: trackObject.track.id,
+				artists: trackObject.track.artists,
+			};
 		});
 
 	//Group by artist
@@ -28,10 +32,10 @@ export async function spotifyShuffle2014(id, total) {
 		const n = shuffledGroups[artist].length;
 		const initialOffset = uniformRandom(0, 1 / n);
 		newOrderOfTracks.push(
-			shuffledGroups[artist].map(({ artist, id }, index) => {
-				const offset = uniformRandom(-0.1 / n, 0.1 / n);
+			shuffledGroups[artist].map(({ artist, id, artists }, index) => {
+				const offset = uniformRandom(-0.2 / n, 0.2 / n);
 				const v = index / n + initialOffset + offset;
-				return { artist: artist, id: id, v: v };
+				return { artist: artist, id: id, v: v, artists: artists };
 			}),
 		);
 		newOrderOfTracks = newOrderOfTracks.flat(1);
@@ -41,7 +45,7 @@ export async function spotifyShuffle2014(id, total) {
 	});
 
 	let temp = newOrderOfTracks.map((track) => {
-		return { artist: track.artist, id: track.id };
+		return { artist: track.artist, id: track.id, artists: track.artists };
 	});
 
 	console.log(temp);
