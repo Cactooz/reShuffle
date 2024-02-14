5ee45f42417a5d03ef3681adefb2a400bd92ec28
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2024-02-14 14:24:28 +0100

    fix fetch more than 100 songs

--- src/fetch.js
+++ src/fetch.js
@@ -47,8 +47,6 @@ export async function fetchPlaylists() {
 					.map((item) => mapPlaylists(item));
 
 				function mapPlaylists(item) {
-					//console.log(item.tracks);
-					fetchTracksOfPlaylist(item.id, item.tracks.total);
 					return {
 						id: item.id,
 						image: item.images.length > 1 ? item.images[1]?.url : item.images[0]?.url,
@@ -73,16 +71,20 @@ export async function fetchTracksOfPlaylist(id, total) {
 			queryKey: id,
 			queryFn: async () => {
 				let offset = 0;
-				let limit = total > 100 ? 100 : total;
 				let fields =
 					'items(track(name, id, artists, is_local, duration_ms, external_urls, album(images)))';
 				let json;
-				const result = await fetch(
-					`https://api.spotify.com/v1/playlists/${id}/tracks?offset=${offset}&limit=${limit}&fields=${fields}`,
-					{ method: 'GET', headers: { Authorization: `Bearer ${token}` } },
-				);
-				json = await result.json();
-				return json.items;
+				let items = [];
+				do {
+					const result = await fetch(
+						`https://api.spotify.com/v1/playlists/${id}/tracks?offset=${offset}&fields=${fields}`,
+						{ method: 'GET', headers: { Authorization: `Bearer ${token}` } },
+					);
+					json = await result.json();
+					items = [...items, ...json.items];
+					offset += 100;
+				} while (offset < total);
+				return items;
 			},
 		}))
 	);
