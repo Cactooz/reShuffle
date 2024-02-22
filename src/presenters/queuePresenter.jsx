66213c3a830f2c05d04f859422d0ae7f748b2014
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2024-02-22 09:13:06 +0100

    Send current playing song to queue view

--- src/presenters/queuePresenter.jsx
+++ src/presenters/queuePresenter.jsx
@@ -6,5 +6,5 @@ export default observer(function QueuePresenter({ model }) {
 	const queue = [...model.queue];
 	queue?.splice(0, model.currentQueueTrack);
 
-	return <QueueView queue={queue} />;
+	return <QueueView queue={queue} song={model.playing} />;
 });
