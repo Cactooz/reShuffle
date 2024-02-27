ab67e805cad9ce800302b463d0381be9ba571d76
Hugo <54435874+Cactooz@users.noreply.github.com>
2024-02-27 20:42:40 +0100

    Add base for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,64 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-
-	build: {
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'reShuffle',
+				short_name: 'reShuffle',
+				description:
+					'A project dedicated to refining music shuffle algorithms to enhance the user experience.',
+				icons: [
+					{
+						src: './public/images/reshuffle-icon-64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: './public/images/reshuffle-icon-192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: './public/images/reshuffle-icon-512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: './public/images/reShuffle-icon-background.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+					{
+						src: './public/images/reShuffle-icon.svg',
+						sizes: 'any',
+					},
+				],
+				theme_color: '#101212',
+				background_color: '#101212',
+				start_url: 'https://reshuffle.one',
+				display: 'standalone',
+				orientation: 'portrait',
+			},
+		}),
+	],
+	/*build: {
 		rollupOptions: {
 			output: {
-				assetFileNames: (assetInfo) => {
-					const extension = assetInfo.name.split('.').pop();
+				assetFileNames: (fileInfo) => {
+					const extension = fileInfo.name.split('.').pop();
 					if (['png', 'jpg', 'jpeg', 'webp', 'svg'].includes(extension.toLowerCase()))
-						return `assets/images/${assetInfo.name}`;
+						return `images/${fileInfo.name}`;
 
-					return assetInfo.name;
+					return fileInfo.name;
 				},
 			},
 		},
-	},
+	},*/
 });
