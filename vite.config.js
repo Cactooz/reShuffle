b44028a6ed921c97c4005ecac395b7c398909309
Hugo <54435874+Cactooz@users.noreply.github.com>
2024-02-27 15:00:00 +0100

    Add HTML meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					const extension = assetInfo.name.split('.').pop();
+					if (['png', 'jpg', 'jpeg', 'webp', 'svg'].includes(extension.toLowerCase()))
+						return `assets/images/${assetInfo.name}`;
+
+					return assetInfo.name;
+				},
+			},
+		},
+	},
+});
