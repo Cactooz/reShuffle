530256afe0e0d7b2bfafb4f0d6c3bcff220901d0
Hugo <54435874+Cactooz@users.noreply.github.com>
2024-02-29 15:01:11 +0100

    Fix wrong base / file paths

--- src/views/errorView.jsx
+++ src/views/errorView.jsx
@@ -6,7 +6,7 @@ export default function errorView() {
 	const navigate = useNavigate();
 	return (
 		<main className='error'>
-			<img src='images/reShuffle-full-white.svg' />
+			<img src='/images/reShuffle-full-white.svg' />
 			<h1>Error 404</h1>
 			<h2>The page you were looking for could not be found :(</h2>
 			<button
