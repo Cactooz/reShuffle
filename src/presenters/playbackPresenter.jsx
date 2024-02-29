95b1f2df2624f137f9097942388e4ff499d8070c
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2024-02-29 09:26:03 +0100

    Add conditional playback functions

--- src/presenters/playbackPresenter.jsx
+++ src/presenters/playbackPresenter.jsx
@@ -3,38 +3,40 @@ import { useEffect } from 'react';
 
 import PlaybackView from '../views/playbackView';
 
-import { playPlaylist } from '../fetch';
+import { playNext, playPause, playPlaylist, playPrevious } from '../fetch';
 
 export default observer(function playbackPresenter({ model }) {
-	async function setPlayPause() {
-		model.setExecutingPlayPause(true);
-		model.player.togglePlay().then(() => {
-			setTimeout(() => model.setExecutingPlayPause(false), 500);
-		});
-	}
-
 	useEffect(() => {
 		navigator.mediaSession.setActionHandler('previoustrack', () => setPrevious());
 		navigator.mediaSession.setActionHandler('nexttrack', () => setNext());
 	}, []);
 
+	async function setPlayPause() {
+		model.setExecutingPlayPause(true);
+		if (model.local) model.player.togglePlay().then();
+		else playPause(model).then();
+		setTimeout(() => model.setExecutingPlayPause(false), 500);
+	}
+
 	function setNext() {
 		model.setExecutingNext(true);
-		if (model.currentQueueTrack !== model.queue.length - 1)
-			model.player.nextTrack().then(() => {
-				setTimeout(() => model.setExecutingNext(false), 500);
-			});
-		else playPlaylist(model.currentPlaylistId, model.queue.length, model);
+		if (model.local) model.player.nextTrack().then();
+		else playNext(model).then();
+		setTimeout(() => model.setExecutingNext(false), 500);
 	}
 
 	async function setPrevious() {
 		model.setExecutingPrevious(true);
-		if (model.currentQueueTrack !== 0) {
+		if (model.local) {
 			model.player.previousTrack().then(() => {
 				model.decrementCurrentQueueTrack();
-				setTimeout(() => model.setExecutingPrevious(false), 500);
+			});
+		} else {
+			playPrevious(model).then(() => {
+				model.decrementCurrentQueueTrack();
 			});
 		}
+		setTimeout(() => model.setExecutingPrevious(false), 500);
 	}
 
 	return (
